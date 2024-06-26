92c821641887f29211832bedb96372c671843dff
striani stefano <45311309+stesto123@users.noreply.github.com>
2024-06-26 13:33:29 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray('.scroll-section').forEach(section => {
+        gsap.to(section, {
+            scrollTrigger: {
+                trigger: section,
+                start: 'top 80%',
+                toggleActions: 'play none none none'
+            },
+            opacity: 1,
+            y: 0,
+            duration: 1
+        });
+    });
+});
